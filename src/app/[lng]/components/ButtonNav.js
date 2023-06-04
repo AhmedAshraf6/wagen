555418813f99d9913008971bd61369af224d5a07
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-06-04 18:14:05 +0200

    aS

--- src/app/[lng]/components/ButtonNav.js
+++ src/app/[lng]/components/ButtonNav.js
@@ -33,7 +33,7 @@ export default function ButtonNav({ navitems, lng }) {
         ></span>
       </button>
       {isOpen ? (
-        <div className='min-w-[70vw] flex flex-col items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-black/90 dark:bg-white/75 rounded-lg backdrop-blur-md py-32'>
+        <div className='min-w-[70vw] flex flex-col items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-black rounded-lg backdrop-blur-md py-32'>
           <ul className='flex items-center flex-col justify-center gap-3 text-white'>
             {navitems &&
               navitems.map((item, index) => (
