acc5ee9fdc12d5f2b23a0e52726eb2b0224513ab
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-06-06 23:21:19 +0200

    add aos animation

--- src/app/[lng]/components/Menu.js
+++ src/app/[lng]/components/Menu.js
@@ -1,20 +1,34 @@
-import { useTranslation } from '@/app/i18n';
+'use client';
+import React from 'react';
+
+// for animation on scroll
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
 import Image from 'next/image';
 import img1 from '../../../../public/menu/img1.svg';
 import img2 from '../../../../public/menu/img2.svg';
 import img3 from '../../../../public/menu/img3.svg';
 import img4 from '../../../../public/menu/img4.svg';
 import img5 from '../../../../public/menu/img5.svg';
-
-const Menu = async ({ lng }) => {
-  const { t } = await useTranslation(lng, 'menu');
+import { useTranslation } from '@/app/i18n/client';
+const Menu = ({ lng }) => {
+  const { t } = useTranslation(lng, 'menu');
   const [m1, m2, m3, m4, m5] = t('meals', { returnObjects: true });
-
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div className='bg-[#F3F4F4] w-full px-5 md:px-15 lg:px-20  pt-32 pb-20 my-4'>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-24'>
         {/* first Meal */}
-        <div className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'>
+        <div
+          className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'
+          data-aos='fade-up'
+          data-aos-delay='50'
+          data-aos-duration='5000'
+        >
           <div className='flex items-center -mt-32 relative '>
             <div className='bg-[#101A24] text-white text-lg rounded-full px-3 py-4 absolute -right-[30px] '>
               Rs{m1.salary}
@@ -32,7 +46,13 @@ const Menu = async ({ lng }) => {
             <p className='text-black text-lg mt-2'>{m1.disc}</p>
           </div>
         </div>
-        <div className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'>
+
+        <div
+          className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'
+          data-aos='fade-up'
+          data-aos-delay='100'
+          data-aos-duration='5000'
+        >
           <div className='flex items-center -mt-32 relative '>
             <div className='bg-[#101A24] text-white text-lg rounded-full px-3 py-4 absolute -right-[30px] '>
               Rs{m2.salary}
@@ -50,7 +70,12 @@ const Menu = async ({ lng }) => {
             <p className='text-black text-lg mt-2'>{m2.disc}</p>
           </div>
         </div>
-        <div className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'>
+        <div
+          className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'
+          data-aos='fade-up'
+          data-aos-delay='200'
+          data-aos-duration='5000'
+        >
           <div className='flex items-center -mt-32 relative '>
             <div className='bg-[#101A24] text-white text-lg rounded-full px-3 py-4 absolute -right-[30px] '>
               Rs{m3.salary}
@@ -68,7 +93,12 @@ const Menu = async ({ lng }) => {
             <p className='text-black text-lg mt-2'>{m3.disc}</p>
           </div>
         </div>
-        <div className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'>
+        <div
+          className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'
+          data-aos='fade-up'
+          data-aos-delay='300'
+          data-aos-duration='5000'
+        >
           <div className='flex items-center -mt-32 relative '>
             <div className='bg-[#101A24] text-white text-lg rounded-full px-3 py-4 absolute -right-[30px] '>
               Rs{m4.salary}
@@ -86,7 +116,12 @@ const Menu = async ({ lng }) => {
             <p className='text-black text-lg mt-2'>{m4.disc}</p>
           </div>
         </div>
-        <div className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'>
+        <div
+          className='flex  flex-col pt-4 pb-20 justify-between items-center rounded-3xl border-[#fff] border-2 px-4'
+          data-aos='fade-up'
+          data-aos-delay='400'
+          data-aos-duration='5000'
+        >
           <div className='flex items-center -mt-32 relative '>
             <div className='bg-[#101A24] text-white text-lg rounded-full px-3 py-4 absolute -right-[30px] '>
               Rs{m5.salary}
