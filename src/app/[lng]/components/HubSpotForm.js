ec164fae9105dbffc648867d86dbd5b434d70773
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-06-10 01:13:14 +0200

    a

--- src/app/[lng]/components/HubSpotForm.js
+++ src/app/[lng]/components/HubSpotForm.js
@@ -1,32 +1,19 @@
 'use client';
+import Script from 'next/script';
 import React, { useEffect } from 'react';
 
 const HubspotContactForm = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://js.hsforms.net/forms/v2.js';
-    document.body.appendChild(script);
-
-    script.addEventListener('load', () => {
-      // @TS-ignore
-      if (window.hbspt) {
-        // @TS-ignore
-        window.hbspt.forms.create({
-          portalId: '139691288',
-          formId: '3355dc76-496c-4f5d-aa84-5a348c6bb49f',
-          target: '#hubspotForm',
-        });
-      }
-    });
-  }, []);
-
   return (
-    <div>
+    <>
+      <Script async src='https://js.hsforms.net/forms/v2.js' />
+      <Script id='show-banner' strategy='lazyOnload'>
+        {`window.hbspt.forms.create({portalId: '139691288',formId:'3355dc76-496c-4f5d-aa84-5a348c6bb49f',target: '#hubspotForm'});`}
+      </Script>
       <div
         id='hubspotForm'
         className='w-[90%] sm:w-[75%] md:w-[50%] flex justify-center mx-auto'
       ></div>
-    </div>
+    </>
   );
 };
 
