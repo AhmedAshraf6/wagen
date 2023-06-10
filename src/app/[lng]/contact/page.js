eeb80ee70aea9340d75aa5c917603e8b32e6407d
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-06-10 02:18:06 +0200

    as

--- src/app/[lng]/contact/page.js
+++ src/app/[lng]/contact/page.js
@@ -1,33 +1,13 @@
-'use client';
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense } from 'react';
+import Loading from '../Loading';
+const HubSpotForm = lazy(() => import('../components/HubSpotForm'));
 
-const contact = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://js.hsforms.net/forms/v2.js';
-    document.body.appendChild(script);
-
-    script.addEventListener('load', () => {
-      // @TS-ignore
-      if (window.hbspt) {
-        // @TS-ignore
-        window.hbspt.forms.create({
-          portalId: '139691288',
-          formId: '3355dc76-496c-4f5d-aa84-5a348c6bb49f',
-          target: '#hubspotForm',
-        });
-      }
-    });
-  }, []);
-
+export default async function Page({ params: { lng } }) {
   return (
-    <div>
-      <div
-        id='hubspotForm'
-        className='w-[90%] sm:w-[75%] md:w-[50%] flex justify-center mx-auto'
-      ></div>
-    </div>
+    <main className={lng === 'en' ? 'font-eng' : 'font-arb'}>
+      <Suspense fallback={<Loading />}>
+        <HubSpotForm />
+      </Suspense>
+    </main>
   );
-};
-
-export default contact;
+}
