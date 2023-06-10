514dc5946949037940dbbf0cd10db93232d1bef6
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-06-10 02:37:01 +0200

    asd

--- src/app/[lng]/components/Navbar.js
+++ src/app/[lng]/components/Navbar.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { LangSwitcher } from './LangSwitcher';
 import { useTranslation } from '@/app/i18n';
 import ButtonNav from './ButtonNav';
+import Link from 'next/link';
 
 const Navbar = async ({ lng }) => {
   const { t } = await useTranslation(lng, 'hero');
@@ -14,23 +15,27 @@ const Navbar = async ({ lng }) => {
     <header className='w-full px-5 md:px-14 lg:px-20 py-4 sm:py-6 font-medium  dark:text-light'>
       {/* small screen */}
       <div className='w-full flex lg:hidden items-center justify-between '>
-        <Image
-          src={logo}
-          width={150}
-          height={150}
-          alt='logo image'
-          className='cursor-pointer w-[120px]'
-        />
+        <Link href={`/${lng}`}>
+          <Image
+            src={logo}
+            width={150}
+            height={150}
+            alt='logo image'
+            className='cursor-pointer w-[120px]'
+          />
+        </Link>
         <ButtonNav navitems={navitems} lng={lng} />
       </div>
       <div className='w-full hidden lg:flex items-center justify-between'>
-        <Image src={logo} alt='logo image' className='cursor-pointer ' />
+        <Link href={`/${lng}`}>
+          <Image src={logo} alt='logo image' className='cursor-pointer ' />
+        </Link>
         <ul className='flex gap-6'>
           {navitems &&
             navitems.map((item, index) => (
               <a
                 key={index}
-                href={`#${item.link}`}
+                href={`${item.link}`}
                 className='text-[18px] hover:text-primary transition-all duration-200 '
               >
                 {item.name}
--- src/app/[lng]/contact/page.js
+++ src/app/[lng]/contact/page.js
@@ -2,9 +2,9 @@ import React, { lazy, Suspense } from 'react';
 import Loading from '../Loading';
 const HubSpotForm = lazy(() => import('../components/HubSpotForm'));
 
-export default async function Page({ params: { lng } }) {
+export default async function Page() {
   return (
-    <main className={lng === 'en' ? 'font-eng' : 'font-arb'}>
+    <main>
       <Suspense fallback={<Loading />}>
         <HubSpotForm />
       </Suspense>
