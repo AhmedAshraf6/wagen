2a8101a7433e94509c0d6642d48f7b35984e7baa
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-06-12 00:15:11 +0200

    as

--- src/middleware.js
+++ src/middleware.js
@@ -15,8 +15,8 @@ export function middleware(req) {
   let lng;
   if (req.cookies.has(cookieName))
     lng = acceptLanguage.get(req.cookies.get(cookieName).value);
-  if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'));
-  if (!lng) lng = fallbackLng;
+  if (!lng) lng = acceptLanguage.get(req.headers.get('ar'));
+  if (!lng) lng = 'ar';
 
   // Redirect if lng in path is not supported
   if (
